c148e295e8022faee6a4e9973d08e2b9214b9cc4
Danny Price <dan@thetelegraphic.com>
2014-01-29 03:14:49 -0500

    Improved output format, now prints in table
    Signed-off-by: Danny Price <dan@thetelegraphic.com>

--- dev/hipsr-check-rms.py
+++ dev/hipsr-check-rms.py
@@ -10,7 +10,7 @@ Compute RMS of digitizers.
 Copyright (c) 2013 The HIPSR collaboration. All rights reserved.
 """
 
-import time, os
+import time, os, sys
 from datetime import datetime
 import numpy as np
 
@@ -28,7 +28,30 @@ __email__    = config.__email__
 __license__  = config.__license__
 __modified__ = datetime.fromtimestamp(os.path.getmtime(os.path.abspath( __file__ )))
 
+
+def color_code(val):
+    """ Color coding RMS values """
+
+    if val < 2:
+        return cprint.red("%2.2f"%val)
+    elif val < 4:
+        return cprint.yellow("%2.2f"%val)
+    elif val > 20:
+        return cprint.red("%2.2f"%val)
+    elif val > 10:
+        return cprint.yellow("%2.2f"%val)
+    else
+        return "%2.2f"%val
+
 if __name__ == '__main__':
+
+    try:
+        # Start in expert mode if any flag is received
+        mode = sys.argv[1]
+        expert_mode = True
+    except IndexError:
+        expert_mode = False
+
     roachlist    = config.roachlist
     katcp_port   = config.katcp_port
 
@@ -47,23 +70,35 @@ if __name__ == '__main__':
     
     # Read RMS levels  
     print cprint.green("\nROACH RMS LEVELS:")
-    print cprint.green("-----------------")
-    for fpga in fpgalist:        
-        levels = katcp_helpers.getSpectrum(fpga, 'rms_levels')
-        if levels['rms_x'] > 50 or levels['rms_y'] > 50:
-            print cprint.red( "%s RMS: %2.2f (A), %2.2f (B)"%(roachlist[fpga.host], levels['rms_x'], levels['rms_y']))
-        else:
-            print "%s RMS: %2.2f (A), %2.2f (B)"%(roachlist[fpga.host], levels['rms_x'], levels['rms_y'])
+    print cprint.green("-----------------\n\n")
 
-    # Read NAR levels
-    print cprint.green("\nROACH NAR LEVELS:")
-    print cprint.green("-----------------")
+    print "    --------------------------"
+    print "    | BEAM |  POL A |  POL B |"
+    print "    |------------------------|"
+
+    to_print = []
     for fpga in fpgalist:        
         levels = katcp_helpers.getSpectrum(fpga, 'rms_levels')
-        n_bits_x, n_bits_y = np.log2(levels['nar_x_on']), np.log2(levels['nar_y_on'])
-        if n_bits_x > 30 or n_bits_y > 30:
-            print cprint.red( "%s NAR power (bits): %2.2f (A), %2.2f (B)"%(roachlist[fpga.host], n_bits_x, n_bits_y))
-        else:     
-            print "%s NAR power (bits): %2.2f (A), %2.2f (B)"%(roachlist[fpga.host], n_bits_x, n_bits_y)
-    
-    
+        rms_x, rms_y = color_code(levels['rms_x']), color_code(levels['rms_y'])
+        to_print.append("    |  %02i  |  %s |  %s |"%(roachlist[fpga.host], rms_x, rms_y)
+
+    to_print.sort()
+
+    for line in to_print:
+        print line
+
+    if expert_mode:
+        # Read NAR levels
+        print cprint.green("\nROACH NAR LEVELS:")
+        print cprint.green("-----------------")
+        for fpga in fpgalist:
+            levels = katcp_helpers.getSpectrum(fpga, 'rms_levels')
+            n_bits_x, n_bits_y = np.log2(levels['nar_x_on']), np.log2(levels['nar_y_on'])
+            if n_bits_x > 30 or n_bits_y > 30:
+                print cprint.red( "%s NAR power (bits): %2.2f (A), %2.2f (B)"%(roachlist[fpga.host], n_bits_x, n_bits_y))
+            else:
+                print "%s NAR power (bits): %2.2f (A), %2.2f (B)"%(roachlist[fpga.host], n_bits_x, n_bits_y)
+
+    # Close connections
+    for fpga in fpgalist:
+        fpga.stop()
